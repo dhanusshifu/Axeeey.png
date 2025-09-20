cdc5b5240e94e8b6272a3a05cab7e38c92334fe8
dhanusshifu <142776007+dhanusshifu@users.noreply.github.com>
2025-09-20 08:12:39 +0530

    Update genx.py

--- core/genx.py
+++ core/genx.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-# Author: Alex a.k.a. VritraSec
+# Author: @ftfarsan
 # Project: LucidX — AI-Powered Image Synth Engine
-# GitHub: https://github.com/VritraSecz
 
 from datetime import datetime
 from time import sleep
